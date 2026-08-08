1d42961565833580f469c84afb39229ee3aa84a2
Michael Ishri <michael.ishri@gmail.com>
2026-08-08 23:53:24 +1200

    Order Homebrew dependencies

--- Formula/cast.rb
+++ Formula/cast.rb
@@ -11,9 +11,9 @@ class Cast < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  depends_on "ffmpeg"
   depends_on "rust" => :build
   depends_on xcode: ["15.0", :build]
+  depends_on "ffmpeg"
   depends_on macos: :ventura
 
   resource "apple-metal" do
